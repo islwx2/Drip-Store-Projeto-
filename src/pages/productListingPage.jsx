f5b3eb517562ee6e75aca0b76b09f4d073e12554
Islane Costa <islanemarafelicio@aluno.unilab.edu.br>
2026-03-11 16:14:05 -0300

    fix: ajusta case-sensitive do import de produtos

--- src/pages/productListingPage.jsx
+++ src/pages/productListingPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Section from '../components/section';
-import ProductListingList from '../components/AbaProdutos/productListingList';
+import ProductListingList from '../components/abaProdutos/productListingList';
 import { produtosPersonalizados } from '../data/products'; 
 import { productsData } from './homePage';
 
